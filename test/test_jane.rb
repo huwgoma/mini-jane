cb94ceacbf37e6d9b46233ab60cace19d194c5eb
Hugo Ma <huwgoma@gmail.com>
2025-05-13 13:29:58 -0400

    Refactoring #create_appointments_cascade - extract into #create_user and #create_profile

--- test/test_jane.rb
+++ test/test_jane.rb
@@ -31,16 +31,51 @@ class TestJane < Minitest::Test
   end
 
   # Admin Schedule Page
+  def test_admin_schedule_default_today
+    skip
+    today = Date.today.to_s
+    yesterday = Date.today.prev_day.to_s
+
+    # Appointment scheduled for today; should be displayed.
+    # - Capture PG::Result object for use in subsequent appointments
+    appointment = create_appointment_cascade(
+                  staff_name: 'Annie Hu', patient_name: 'Hugo Ma', 
+                  datetime: "#{today} 10:00AM", discipline: 'Physiotherapy',
+                  tx_name: 'PT - Treatment', tx_length: 30, tx_price: 85.00
+                 ).first
+
+
+    # Appointment scheduled for yesterday - Should not be displayed
+    # staff_name: 'Annie Hu', patient_name: 'Hugo Ma',
+    # datetime: "#{yesterday} 10:00AM", discipline: 'Physiotherapy',
+    # tx_name: 'PT - Initial', tx_length: 45, tx_price: 100.00)
+
+    get '/admin/schedule/'
+
+    assert_includes(last_response.body, 'PT - Treatment')
+    refute_includes(last_response.body, 'PT - Initial')
+  end
+
   def test_admin_schedule_fixed_date_one_practitioner_one_appointment
     date = '2024-10-08'
 
-    create_appointment_cascade(staff_name: 'Annie Hu', patient_name: 'Hugo Ma',
-                               datetime: "#{date} 10:00AM", discipline: 'Physiotherapy',
-                               tx_name: 'PT - Initial', tx_length: 45, tx_price: 100.00)
+    staff_id = extract_id(create_user('Annie Hu'))
+    create_profile(staff_id, type: 'staff')
+
+    patient_id = extract_id(create_user('Hugo Ma'))
+    create_profile(patient_id)
+
+    # discipline_id = create_discipline('Physiotherapy', clinical: true)
+
+
+    # staff_id, patient_id = create_users('Annie Hu', 'Hugo Ma')
+    binding.pry
+    # create_appointment_cascade(staff_name: 'Annie Hu', patient_name: 'Hugo Ma',
+    #                            datetime: "#{date} 10:00AM", discipline: 'Physiotherapy',
+    #                            tx_name: 'PT - Initial', tx_length: 45, tx_price: 100.00)
 
     get '/admin/schedule/2024-10-08'
 
-    assert_equal(200, last_response.status)
     assert_includes(last_response.body, "<h2>#{date}")
     assert_includes(last_response.body, 'Physiotherapy')
     assert_includes(last_response.body, 'Annie Hu')
@@ -48,14 +83,40 @@ class TestJane < Minitest::Test
   end
 
   private
+  
+  
+  #################################################
+  # Helpers for generating test data before tests #
+  #################################################
+  # Extract the ID from a PG::Result object
+  def extract_id(result)
+    result.first['id'].to_i  
+  end
 
-  # Helpers for generating test data before tests
-  def create_appointment_cascade(staff_name:, patient_name:, 
-                                 datetime:, discipline:,
-                                 tx_name:, tx_length:, tx_price:)
+  # Create a dummy user
+  def create_user(name)
+    first_name, last_name = name.split
+    
+    sql = "INSERT INTO users(first_name, last_name) VALUES($1, $2) RETURNING *;"
+    @storage.query(sql, first_name, last_name)
+  end
+
+  # Create a dummy profile (staff/patient)
+  def create_profile(user_id, type: 'patients')
+    sql = "INSERT INTO #{type} (user_id) VALUES ($1);"
+    @storage.query(sql, user_id)
+  end
+
+  def create_discipline
+  
+  end
+
+
+
+  def create_appointment_cascade(staff_name:, patient_name:, datetime:, 
+                                 discipline:, tx_name:, tx_length:, tx_price:)
     staff_id = insert_user_returning_id(staff_name)
     patient_id = insert_user_returning_id(patient_name)
-
     insert_user_profile(staff_id, table: 'staff')
     insert_user_profile(patient_id, table: 'patients')
 
@@ -63,7 +124,7 @@ class TestJane < Minitest::Test
     treatment_id = insert_treatment_returning_id(tx_name, discipline_id, tx_length, tx_price)
     insert_staff_discipline(staff_id, discipline_id)
     
-    insert_appointment(staff_id, patient_id, datetime, treatment_id)
+    insert_and_return_appointment(staff_id, patient_id, datetime, treatment_id)
   end
 
   def insert_user_returning_id(full_name)
@@ -104,9 +165,9 @@ class TestJane < Minitest::Test
     @storage.query(sql, staff_id, discipline_id)
   end
 
-  def insert_appointment(staff_id, patient_id, datetime, treatment_id)
+  def insert_and_return_appointment(staff_id, patient_id, datetime, treatment_id)
     sql = "INSERT INTO appointments (staff_id, patient_id, datetime, treatment_id)
-           VALUES($1, $2, $3, $4);"
+           VALUES($1, $2, $3, $4) RETURNING *;"
     @storage.query(sql, staff_id, patient_id, datetime, treatment_id)
   end
 end
